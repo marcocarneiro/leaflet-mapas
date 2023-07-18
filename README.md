ab82c75c3da38719b1d6221af153f2292dc78c16
marcocarneiro <marco.carneiro7@gmail.com>
2023-07-17 22:17:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# leaflet-mapas
+ 
---
+++ codeRota.js
@@ -0,0 +1,13 @@
+var map = L.map('map');
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+L.Routing.control({
+    waypoints: [
+        L.latLng(57.74, 11.94),
+        L.latLng(57.6792, 11.949)
+    ],
+    routeWhileDragging: true
+}).addTo(map);
